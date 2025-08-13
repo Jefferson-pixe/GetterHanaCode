9f68356f045730aa5bc4012770c1473e7f91639f
jefferson-pixe <practicante_tecnologia@grupo-aurum.com>
2025-08-13 16:54:25 -0600

    Actualización automática SPs 2025-08-13 16:54:25

--- SBO_BLINTEC/SBO_BLINTEC.SBO_SP_POSTTRANSACTIONNOTICE.sql
+++ SBO_BLINTEC/SBO_BLINTEC.SBO_SP_POSTTRANSACTIONNOTICE.sql
@@ -32,30 +32,8 @@ error_message := N'Ok';
 --------------------------------------------------------------------------------------------------------------------------------
 
 
-/*-------------------------Addon de correos---------------------------------*/
-IF (:object_type = '46'  AND :transaction_type='A')
-THEN
 
-	SELECT
-	T0."CardName",T0."CardCode",T0."DocNum",T0."DocDate"
-	INTO C_CardName,C_CardCode,C_DocNum,C_DocDate
-	FROM "SBO_BLINTEC"."OVPM" T0
-	INNER JOIN "SBO_BLINTEC"."@CONFI_CORREO" T1 ON T1."U_Activo"='Y'
-	INNER JOIN "SBO_BLINTEC"."OCRD" T2 ON T0."CardCode"=T2."CardCode" AND IFNULL(T2."E_Mail",'')!=''
-	WHERE
-	T0."DataSource"='A' AND
-	T0."Status"='Y' AND
-	T0."DocEntry"= :list_of_cols_val_tab_del;
-
-	IF(IFNULL(:C_DocNum,0)!=0)
-	THEN
-		INSERT INTO "SBO_BLINTEC"."@LOG_CORREOS" ("Code","Name","U_CardCode","U_DocEntryP","U_DocNumP","U_Fecha","U_Comentarios")
-		VALUES ("SBO_BLINTEC".CORREOS_S.NEXTVAL,:C_CardName,:C_CardCode,:list_of_cols_val_tab_del,:C_DocNum,:C_DocDate,null);
-	END IF;
-
-END IF;
-/*-------------------------Fin Addon de correos---------------------------------*/
-
+/***************************** TEST *******************************************/
 
 --------------------------------------------------------------------------------------------------------------------------------
 
